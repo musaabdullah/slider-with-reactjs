4c539698615568252cad4d4dca6345be461e4d6d
musaabdullah <musaabdalwahed@gmail.com>
2021-03-10 15:55:20 +0200

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import  {useState} from 'react';
+import { SliderData } from './imageslider';
 import './App.css';
 
+
+// const length =  slides.length;
 function App() {
+  const [current, setCurrent] = useState(0);
+
+  const length = SliderData.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+    console.log(current);
+  }
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+    console.log(current);
+  }
+
+
+  if(!Array.isArray(SliderData) || SliderData.length <= 0){
+    return null;
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="slider">
+        <i className="left-arrow fa fa-toggle-left" onClick={prevSlide}></i>
+        <i className="right-arrow fa fa-toggle-right" onClick={nextSlide}></i>
+      {
+        SliderData.map((slide, index) => {
+          return  <div className={index === current ? 'slide active' : 'slide'} key={index}>
+                    {index === current && (<img src={slide.image} className="image" />)}
+          </div>
+        } )
+      }
+      </section>
     </div>
   );
 }
---
+++ src/imageslider.js
@@ -0,0 +1,14 @@
+ export const SliderData = [
+    {
+        image: "https://lh3.googleusercontent.com/-DrUHoty_hP4/VuDTsKkIy5I/AAAAAAAAA24/MwtuNsVXdDM/s1600/Ankit%252520edit94386.jpg"
+    },
+    {
+        image: "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg",
+    },
+    {
+        image: "https://www.nhm.ac.uk/content/dam/nhmwww/discover/wpy-2020-winning-images/wpy-winning-image-full-width.jpg",
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2014/02/27/16/10/tree-276014__340.jpg",
+    }
+]
